48d95b1b4dcb37ded63bbb21b01b1e8543a2d723
shreyas patil <shreyaspatil1901@gmail.com>
2023-05-02 11:57:53 +0530

    IFRAME

--- src/App.js
+++ src/App.js
@@ -7,10 +7,18 @@ function App() {
     <div className="App">
       <header className="App-header">Dashboard</header>
       <Iframe
+        className="iframe-container"
         width="600"
         height="400"
-        seamless
-        src="http://18.234.97.196:3000/superset/explore/?r=58&standalone=true&height=400"
+        url="http://18.234.97.196:3000/superset/explore/?r=58&standalone=true&height=400"
+      ></Iframe>
+      <Iframe
+        className="iframe-container"
+        width="600"
+        height="400"
+        frameBorder="0"
+        // scrolling="no"
+        src="http://18.234.97.196:3000/superset/explore/?r=60&standalone=true&height=400"
       ></Iframe>
     </div>
   );
